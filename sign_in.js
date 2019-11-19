a736d37700f0440a38b8252d6a87106871eddca6
Cassidy Soutter <cassidysoutter@gmail.com>
2019-11-19 13:12:45 -0500

    Added sign in and sign up pages with the start of the js
    Added sign in and sign up pages with the start of the js

---
+++ sign_in.js
@@ -0,0 +1,46 @@
+export const handleCancelSignIn = function(event) {
+    //go back to index.html
+}
+
+export const handleSubmitSignIn = function(event) {
+    //go back to index.html with proper permissions or throw error/alert message
+}
+
+export const loadSignInForm = function() {
+    // Grab a jQuery reference to the root HTML element
+    const $root = $('#root');
+    const form = `
+    <form class="container"action="action_page.php" method="post">
+        <div class="container imgcontainer">
+            <img src="pictures/user.png" alt="Avatar" class="avatar">
+        </div>
+    
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        
+            <button class="button_signIn" type="submit" id="signInButton">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+    
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" class="button cancelbtn" id="cancelButton">Cancel</button>
+            <span class="psw">Forgot <a href="#">Password?</a></span>
+            <span class="psw" style="padding-right: 20px;"><a href="sign_up.html">Create New Account</a></span>
+        </div>
+    </form>
+    `;
+    $root.append(form);
+    //set listeners for each added element
+    $(`#signInButton`).on("click", null, room, handleSubmitSignIn);
+    $(`#cancelButton`).on("click", null, room, handleCancelSignIn);
+};
+
+$(function() {
+    loadSignInForm();
+});
---
+++ sign_up.js
@@ -0,0 +1,50 @@
+export const handleCancelSignIn = function(event) {
+    //go back to index.html
+}
+
+export const handleSubmitSignIn = function(event) {
+    //go back to index.html with proper permissions or throw error/alert message
+}
+
+export const loadSignInForm = function() {
+    // Grab a jQuery reference to the root HTML element
+    const $root = $('#root');
+    const form = `
+    <form class="container"action="action_page.php" method="post">
+        <div class="container imgcontainer">
+            <img src="pictures/user.png" alt="Avatar" class="avatar">
+        </div>
+    
+        <div class="container">
+            <label for="uname"><b>First Name</b></label>
+            <input type="text" placeholder="Enter First Name" name="first" required>
+
+            <label for="uname"><b>Last Name</b></label>
+            <input type="text" placeholder="Enter Last Name" name="last" required>
+
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+        
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+        
+            <button class="button_signIn" type="submit">Create New Account</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+    
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" class="button cancelbtn" id="cancelButton">Cancel</button>
+        </div>
+    </form>
+    `;
+    $root.append(form);
+    //set listeners for each added element
+    $(`#signInButton`).on("click", null, room, handleSubmitSignIn);
+    $(`#cancelButton`).on("click", null, room, handleCancelSignIn);
+};
+
+$(function() {
+    loadSignInForm();
+});
