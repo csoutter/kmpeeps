fd842dae877836247e859c4ab650a18e2d497919
Jessica Davis <jesmaddav@live.com>
2019-11-19 20:26:41 -0500

    added axios

--- sign_up.js
+++ sign_up.js
@@ -6,6 +6,19 @@ export const handleSubmitSignIn = function(event) {
     //go back to index.html with proper permissions or throw error/alert message
 }
 
+export const handleCreateAccount = function(event) {
+    //go back to index.html with proper permissions or throw error/alert message
+
+    event.preventDefault();
+    
+    const response = await axios({
+        method: "post",
+        url: "http://localhost:3000/account/create",
+        withCredentials: true,
+        body: {}
+    });
+}
+
 export const loadSignInForm = function() {
     // Grab a jQuery reference to the root HTML element
     const $root = $('#root');
@@ -28,7 +41,7 @@ export const loadSignInForm = function() {
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
         
-            <button class="button_signIn" type="submit">Create New Account</button>
+            <button class="button_signIn" type="submit" id="createAccountButton"<Create New Account</button>
             <label>
                 <input type="checkbox" checked="checked" name="remember"> Remember me
             </label>
@@ -43,6 +56,7 @@ export const loadSignInForm = function() {
     //set listeners for each added element
     $(`#signInButton`).on("click", null, room, handleSubmitSignIn);
     $(`#cancelButton`).on("click", null, room, handleCancelSignIn);
+    $(`#createAccountButton`).on("click", null, room, handleCreateAccount)
 };
 
 $(function() {
